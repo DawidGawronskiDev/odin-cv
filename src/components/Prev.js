af39c05e35d0796f712b4b0a400bd15a72b5e3ab
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2023-07-06 22:58:10 +0100

    Additional commit

--- src/components/Prev.js
+++ src/components/Prev.js
@@ -1,11 +1,50 @@
 import React from "react";
 
-function Prev({ personalInfo, experience }) {
+function Prev({ personalInfo, experience, education }) {
   return (
-    <>
-      <h1>{personalInfo.firstName}</h1>
-      <h1>{experience[0].position}</h1>
-    </>
+    <div className="prev">
+      <div className="cv">
+        <aside>
+          <div>
+            <h1>
+              {personalInfo.firstName} {personalInfo.lastName}
+            </h1>
+          </div>
+
+          <ul>
+            <li>{personalInfo.address}</li>
+            <li>{personalInfo.phone}</li>
+            <li>{personalInfo.email}</li>
+          </ul>
+
+          <div>
+            <h3>Description</h3>
+            <p>{personalInfo.desc}</p>
+          </div>
+        </aside>
+
+        <main>
+          <h3>Experience</h3>
+          <ul>
+            {experience.map((place) => {
+              return (
+                <li>
+                  <span>{place.position}</span>
+                  <div>
+                    <span>
+                      {place.company}, {place.city}
+                    </span>
+                    <span>
+                      {place.from} - {place.to}
+                    </span>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </main>
+      </div>
+    </div>
   );
 }
 
